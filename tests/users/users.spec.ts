e50e169aad8fc5cda72850001afa22410a4d267f
Weiller Carvalho <engweiller@hotmail.com>
2023-03-29 22:08:36 -0300

    test: TDD

--- tests/users/users.spec.ts
+++ tests/users/users.spec.ts
@@ -1,7 +1,11 @@
 import { test } from '@japa/runner'
+import supertest from 'supertest'
+
+const BASE_URL = `http://${process.env.HOST}:${process.env.PORT}`
 
 test.group('User', () => {
-  test('it should create an user', ({ assert }) => {
-    assert.isTrue(true)
-  })
+  test('it should create an user', async () => {
+    const userData = { email: 'weiller@test.com', username: 'weiller', password: 'test' }
+    await supertest(BASE_URL).post('/users').send(userData).expect(201)
+  }).pin()
 })
