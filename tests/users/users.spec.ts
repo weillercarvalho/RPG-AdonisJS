6ac8185c9f31e7bb049293ff02b62b877dc0fca3
Weiller Carvalho <engweiller@hotmail.com>
2023-04-02 18:54:52 -0300

    fix: The process.env.PORT its not recognized.

--- tests/users/users.spec.ts
+++ tests/users/users.spec.ts
@@ -1,12 +1,12 @@
 import { test } from '@japa/runner'
 import supertest from 'supertest'
 
-const BASE_URL = `http://${process.env.HOST}:${process.env.PORT}`
+const BASE_URL = `http://${process.env.HOST}:3333`
+//Resolve the problem of connection cant recognize the 3333 passed through process.env.PORT.
 
 test.group('User', () => {
   test('it should create an user', async () => {
     const userData = { email: 'weiller@test.com', username: 'weiller', password: 'test' }
-    console.log(BASE_URL)
     await supertest(BASE_URL).post('/users').send(userData).expect(201)
   }).pin()
 })
