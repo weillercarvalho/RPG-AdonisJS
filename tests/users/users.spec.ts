a04dff04d323a4086299263bc699d5b7d7b95394
Weiller Carvalho <engweiller@hotmail.com>
2023-04-04 23:47:12 -0300

    test: Testing test

--- tests/users/users.spec.ts
+++ tests/users/users.spec.ts
@@ -13,12 +13,21 @@ test.group('User', () => {
       password: 'test',
       avatar: 'https://i.pinimg.com/originals/df/91/02/df91027b7859de1d4e5d2684604737d5.jpg',
     }
-    const { body } = await supertest(BASE_URL).post('/api/users').send(userData).expect(200)
+    const { body } = await supertest(BASE_URL).post('/api/users').send(userData)
     assert.exists(body, 'User undefined')
     assert.exists(body.id, 'Id undefined')
     assert.equal(body.email, userData.email)
     assert.equal(body.username, userData.username)
     assert.equal(body.password, userData.password)
     assert.equal(body.avatar, userData.avatar)
+  })
+  test('it should return status 404 if email/username/password is empty', async ({ assert }) => {
+    const userData = {
+      username: 'weiller',
+      password: 'test',
+      avatar: 'https://i.pinimg.com/originals/df/91/02/df91027b7859de1d4e5d2684604737d5.jpg',
+    }
+    const { status } = await supertest(BASE_URL).post('/api/users').send(userData)
+    assert.equal(status, 404)
   }).pin()
 })
